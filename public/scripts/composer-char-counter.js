d93f23425adb6f7b4ce5d349af4022566a4822b3
James <jameshuang337@gmail.com>
2021-12-15 16:35:26 -0800

    added counter update functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    // --- our code goes here ---
-  });
+
+    // Updating counter based on text input
+    const $tweetText = $('#tweet-text');
+    $tweetText.on('input', function () {
+        const input = $tweetText.val();
+        const $counter = $('#counter');
+
+        // Making counter red or not
+        if (input.length > 140) {
+            $counter.addClass('red-text')
+        } else {
+            $counter.removeClass('red-text')
+        }
+        $counter.html(140 - input.length)
+        
+        
+    });
+
+});
