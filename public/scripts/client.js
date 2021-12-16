0a273869c603451daf18bbf2f07034facc0f35e9
James <jameshuang337@gmail.com>
2021-12-15 20:35:59 -0800

    added loadTweets function to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,27 +7,21 @@
 // Runs when all HTML elements have been loaded in
 $(document).ready(function () {
 
-    const $tweetText = $('#tweet-text');
     $('#tweet-form').submit(function(event) {
         event.preventDefault();
-
+        let data = $(this).serialize();
         $.ajax({
-            url: 'http://localhost:8080/',
-            method:'POST'
+            url: 'http://localhost:8080/tweets',
+            method:'POST',
+            data: data
         })
         .then((response) => {
-
+            console.log("data was posted")
         })
         .catch((error) => {
             console.log(error)
-        })
-
-
+        });
     });
-
-
-
-
 
     const createTweetElement = (tweet) => {
         const $tweet = $(`
@@ -48,42 +42,51 @@ $(document).ready(function () {
                         <i class="fas fa-heart"></i>
                     </div>
                 </footer>
-
             </article>`);
-
         return $tweet;
-
     }
 
     // Rendering tweets loaded in from an array of tweets
     const renderTweets = function (tweets) {
+        // clearing the .all-tweets section between rendering the array of tweets
         $('.all-tweets').html('');
+        // loops through tweets
+        // calls createTweetElement for each tweet
+        // takes return value and appends it to the tweets container
         for (const tweet of tweets) {
-        
             let $tweet = createTweetElement(tweet);
             // Using jquery to prepend tweets into the relevant section
             $('.all-tweets').prepend($tweet)
         };
-
-        // loops through tweets
-        // calls createTweetElement for each tweet
-        // takes return value and appends it to the tweets container
     };
 
+    // Implementing function to make an AJAX GET request and run renderTweets using the JSON response
+    const loadTweets = function() {
+        $.ajax('http://localhost:8080/tweets', { method:'GET'})
+            .then(function(tweets) {
+                renderTweets(tweets)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }
 
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const tweetData = [{
-        "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png",
-            "handle": "@SirIsaac"
-        },
-        "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-    }]
+    loadTweets();
 
-    renderTweets(tweetData);
+
+    // // Test / driver code (temporary). Eventually will get this from the server.
+    // const tweetData = [{
+    //     "user": {
+    //         "name": "Newton",
+    //         "avatars": "https://i.imgur.com/73hZDYK.png",
+    //         "handle": "@SirIsaac"
+    //     },
+    //     "content": {
+    //         "text": "If I have seen further it is by standing on the shoulders of giants"
+    //     },
+    //     "created_at": 1461116232227
+    // }]
+
+    // renderTweets(tweetData);
 
 });
