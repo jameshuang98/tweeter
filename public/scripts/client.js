1d53122e8da9037868c1ad4b4e0e44b6f15bc68e
James <jameshuang337@gmail.com>
2021-12-16 18:33:08 -0800

    linted script files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,54 +5,53 @@
  */
 
 // Runs when all HTML elements have been loaded in
-$(document).ready(function () {
+$(document).ready(function() {
 
-    // Initializing variable for error message tag
-    const $errorContainer = $("#error-message");
-    $errorContainer.hide();
+  // Initializing variable for error message tag
+  const $errorContainer = $("#error-message");
+  $errorContainer.hide();
 
+  // Event handler for tweet submission
+  $('#tweet-form').on('submit', function(event) {
+    event.preventDefault();
 
-    // Event handler for tweet submission
-    $('#tweet-form').on('submit', function(event) {
-        event.preventDefault();
+    const $tweetText = $('#tweet-text');
+    const $counter = $('#counter');
+    const input = $tweetText.val();
 
-        const $tweetText = $('#tweet-text');
-        const $counter = $('#counter');
-        const input = $tweetText.val();
-
-        // form validation for tweets over 140 characters or 0 characters
-        if (input.length > 140) {
-            $errorContainer.text("Tweet cannot be over 140 characters!")
-            $errorContainer.slideDown();
-        } else if (input.length === 0) {
-            $errorContainer.text("Tweet cannot be empty!")
-            $errorContainer.slideDown();
-        } else {
-            $errorContainer.slideUp();
-            let $data = $(this).serialize();
-            // AJAX post request to /tweets/
-            $.ajax({
-                url: 'http://localhost:8080/tweets',
-                method:'POST',
-                data: $data
-            })
-            .then((response) => {
-                loadTweets();
-                $tweetText.val('');
-                $counter.html(140)
+    // form validation for tweets over 140 characters or 0 characters
+    if (input.length > 140) {
+      $errorContainer.text("Tweet cannot be over 140 characters!");
+      $errorContainer.slideDown();
+    } else if (input.length === 0) {
+      $errorContainer.text("Tweet cannot be empty!");
+      $errorContainer.slideDown();
+    } else {
+      $errorContainer.slideUp();
+      let $data = $(this).serialize();
+      // AJAX post request to /tweets/
+      $.ajax({
+        url: 'http://localhost:8080/tweets',
+        method:'POST',
+        data: $data
+      })
+        .then((response) => {
+          loadTweets();
+          $tweetText.val('');
+          $counter.html(140);
                 
-            })
-            .catch((error) => {
-                const message = (error.responseJSON && error.responseJSON.error) || error.statusText;
-                $errorContainer.text(message);
-                $errorContainer.slideDown();
-                console.log(error)
-            });
-        }
-    });
+        })
+        .catch((error) => {
+          const message = (error.responseJSON && error.responseJSON.error) || error.statusText;
+          $errorContainer.text(message);
+          $errorContainer.slideDown();
+          console.log(error);
+        });
+    }
+  });
 
-    const createTweetElement = (tweet) => {
-        const $tweet = $(`
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`
             <article>
                 <header class="tweet-head">
                     <div class="left-header">
@@ -71,40 +70,40 @@ $(document).ready(function () {
                     </div>
                 </footer>
             </article>`);
-        return $tweet;
+    return $tweet;
+  };
+
+  // Rendering tweets loaded in from an array of tweets
+  const renderTweets = function(tweets) {
+    // clearing the .all-tweets section between rendering the array of tweets
+    $('.all-tweets').html('');
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      // Using jquery to prepend tweets into the relevant section
+      $('.all-tweets').prepend($tweet);
     }
+  };
 
-    // Rendering tweets loaded in from an array of tweets
-    const renderTweets = function (tweets) {
-        // clearing the .all-tweets section between rendering the array of tweets
-        $('.all-tweets').html('');
-        // loops through tweets
-        // calls createTweetElement for each tweet
-        // takes return value and appends it to the tweets container
-        for (const tweet of tweets) {
-            let $tweet = createTweetElement(tweet);
-            // Using jquery to prepend tweets into the relevant section
-            $('.all-tweets').prepend($tweet)
-        };
-    };
+  // Implementing function to make an AJAX GET request and run renderTweets using the JSON response
+  const loadTweets = function() {
+    $.ajax('http://localhost:8080/tweets', { method:'GET'})
+      .then(function(tweets) {
+        renderTweets(tweets);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    // Implementing function to make an AJAX GET request and run renderTweets using the JSON response
-    const loadTweets = function() {
-        $.ajax('http://localhost:8080/tweets', { method:'GET'})
-            .then(function(tweets) {
-                renderTweets(tweets)
-            })
-            .catch((error) => {
-                console.log(error)
-            });
-    }
+  // Helper function that escapes text
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-    // Helper function that escapes text
-    const escape = function(str) {
-        let div = document.createElement("div");
-        div.appendChild(document.createTextNode(str));
-        return div.innerHTML;
-    };
-
-    loadTweets();
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-    // Updating counter based on text input
-    const $tweetText = $('#tweet-text');
-    $tweetText.on('input', function () {
-        const input = $tweetText.val();
-        // Using jquery to get counter element
-        const $counter = $('#counter');
+  // Updating counter based on text input
+  const $tweetText = $('#tweet-text');
+  $tweetText.on('input', function() {
+    const input = $tweetText.val();
+    // Using jquery to get counter element
+    const $counter = $('#counter');
 
-        // Making counter appear red or not
-        if (input.length > 140) {
-            $counter.addClass('red-text')
-        } else {
-            $counter.removeClass('red-text')
-        };
-        $counter.html(140 - input.length);
-    });
+    // Making counter appear red or not
+    if (input.length > 140) {
+      $counter.addClass('red-text');
+    } else {
+      $counter.removeClass('red-text');
+    }
+    $counter.html(140 - input.length);
+  });
 });
 
